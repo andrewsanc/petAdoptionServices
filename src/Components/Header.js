d6facfd82563a3bee92cd76682dc427fade5c070
TB831 <druu831@gmail.com>
2019-08-25 15:34:03 -0700

    Header component built

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="ui large inverted blue menu">
+      <div className="header item">Pet Adoption Services</div>
+    </div>
+  );
+}
+ 
+export default Header;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../Components/Header';
 
 class App extends Component {
   render() { 
     return (
       <div>
-        <p>hello world!</p>
+        <Header />
       </div>
     );
   }
