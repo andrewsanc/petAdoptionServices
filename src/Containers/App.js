6cb3db91e701a21d96d53ad0ddf29049bcc430f5
TB831 <druu831@gmail.com>
2019-08-25 17:23:11 -0700

    rendering cards

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import Animals from '../Components/Animals'
+import { dogs } from '../assets/data/dogs.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: dogs,
+      selected: null
+    }
+  }
+
+  clickHandler = (index) => {
+    console.log('click', index);
+  }
+
   render() { 
     return (
       <div>
         <Header />
+        <Animals dogs={dogs} clickHandler={this.clickHandler} />
         <Footer />
       </div>
     );
